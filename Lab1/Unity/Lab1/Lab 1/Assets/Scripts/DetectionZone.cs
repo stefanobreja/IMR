cef1d323a14c642c042a95dccd46061d4546a511
stefanobreja <stefanobreja@gmail.com>
2022-10-30 22:21:43 +0200

    lab 1,2,3 updates

--- Lab1/Unity/Lab1/Lab 1/Assets/Scripts/DetectionZone.cs
+++ Lab1/Unity/Lab1/Lab 1/Assets/Scripts/DetectionZone.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// for detection
 public class DetectionZone : MonoBehaviour
 {
     public string tagEnemy = "enemy";
--- Lab1/Unity/Lab1/Lab 1/Assets/Scripts/PlayerBehaviour.cs
+++ Lab1/Unity/Lab1/Lab 1/Assets/Scripts/PlayerBehaviour.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// for animation
 public class PlayerBehaviour : DetectionZone
 {
     private Animator mAnimator;
